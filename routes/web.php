db058ce79417799243dbf0dde94f6e8768ae9dab
dcastanho44 <dcastanho44@gmail.com>
2022-02-12 02:53:16 -0300

    #152 - implementando cadastro (1)

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,8 @@ Route::prefix('/app')->
     Route::get('/cliente', 'ClienteController@index')->name('app.cliente'); 
 
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+    Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
+    Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
 
     Route::get('/produto', 'ProdutoController@index')->name('app.produto');
 
