8ffc5e4fedc10edea2ed860ba6644b0a776b834c
dcastanho44 <dcastanho44@gmail.com>
2022-02-12 05:10:07 -0300

    updates!

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -26,7 +26,8 @@ class FornecedorController extends Controller
 
         $msg = ''; //mensagem começa vazia mas terá conteúdo caso o cadastro seja realizado com sucesso
         
-        if($request->input('_token') != ''){ //o token aparece na requisição por causa do GET, caso esteja vazio então podemos cadastrar
+        //inserção
+        if($request->input('_token') != '' && $request->input('id') == ''){ //o token aparece na requisição por causa do GET, caso não esteja vazio então podemos cadastrar, o ID estando vazio significa que é uma adição de dados
             //validação
             $regras = [
                 'nome' => 'required|min:3|max:40',
@@ -50,7 +51,28 @@ class FornecedorController extends Controller
             $fornecedor->create($request->all()); //inserindo dados na tabela
 
             $msg = 'Cadastro realizado com sucesso';
-        } 
+        }
+            //edição
+            if($request->input('_token') != '' && $request->input('id') != '') { //caso haja um id inserido significa que uma edição será feita
+                $fornecedor = Fornecedor::find($request->input('id'));
+                $update = $fornecedor->update($request->all());
+
+                if($update) {
+                    $msg = 'Update realizado com sucesso';
+                } else {
+                    $msg = 'Update não realizado';
+                }
+
+                return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]); //redireciona junto com a mensagem
+            }
+        
         return view('app.fornecedor.adicionar', ['msg' => $msg]);
+    }
+
+
+    
+    public function editar($id, $msg = ''){
+        $fornecedor = Fornecedor::find($id);
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]); //manda os dados a serem editados para a view adicionar
     }
 }
