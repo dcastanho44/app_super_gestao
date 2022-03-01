463008eae3a5c22262e42d8df9a506fe5cacb1d1
dcastanho44 <dcastanho44@gmail.com>
2022-03-01 19:11:27 -0300

    #192

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Pedido;
+use App\Cliente;
 
 class PedidoController extends Controller
 {
@@ -15,7 +16,7 @@ class PedidoController extends Controller
     public function index(Request $request)
     {
         $pedidos = Pedido::paginate(10);
-        return view('app.pedido.index', ['pedidos' => $pedidos, 'request' => $request->all()]);
+        return view('app.pedido.index', ['pedidos' => $pedidos, 'request' => $request->all()] );
     }
 
     /**
@@ -25,7 +26,8 @@ class PedidoController extends Controller
      */
     public function create()
     {
-        //
+        $clientes = Cliente::all();
+        return view('app.pedido.create', ['clientes' => $clientes]);
     }
 
     /**
@@ -36,7 +38,21 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $regras = [
+            'cliente_id' => 'exists:clientes,id'
+        ];
+
+        $feedback = [
+            'cliente_id.exists' => 'O cliente informado não existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        $pedido = new Pedido();
+        $pedido->cliente_id = $request->get('cliente_id');
+        $pedido->save();
+
+        return redirect()->route('pedido.index');
     }
 
     /**
--- resources/views/app/pedido/index.blade.php
+++ resources/views/app/pedido/index.blade.php
@@ -1,8 +1,8 @@
 @extends('app.layouts.basico')
 
-@section ('titulo', 'Pedido')
+@section('titulo', 'Pedido')
 
-@section ('conteudo')
+@section('conteudo')
     
     <div class="conteudo-pagina">
 
@@ -16,56 +16,58 @@
                 <li><a href="">Consulta</a></li>
             </ul>
         </div>
-        
+
         <div class="informacao-pagina">
             <div style="width: 90%; margin-left: auto; margin-right: auto;">
                 <table border="1" width="100%">
                     <thead>
                         <tr>
-                            <th>ID do Pedido</th>
+                            <th>ID Pedido</th>
                             <th>Cliente</th>
                             <th></th>
                             <th></th>
+                            <th></th>
                         </tr>
-                    </thead>
+                    </head>
 
                     <tbody>
                         @foreach($pedidos as $pedido)
                             <tr>
                                 <td>{{ $pedido->id }}</td>
                                 <td>{{ $pedido->cliente_id }}</td>
-                                <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id]) }}">Visualizar</a></td>
+                                <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id ]) }}">Visualizar</a></td>
                                 <td>
-                                    <form id="form_{{$pedido->id}}" method="post" action="{{ route('pedido.destroy', ['pedido' => $pedido->id])}}">
+                                    <form id="form_{{$pedido->id}}" method="post" action="{{ route('pedido.destroy', ['pedido' => $pedido->id]) }}">
                                         @method('DELETE')
                                         @csrf
                                         <!--<button type="submit">Excluir</button>-->
-                                        <a href="#" onClick="document.getElementbyId('form_{{$pedido->id}}').submit()">Excluir</a>
-                                        
+                                        <a href="#" onclick="document.getElementById('form_{{$pedido->id}}').submit()">Excluir</a>
                                     </form>
                                 </td>
-                                <td><a href="{{ route('pedido.edit', ['pedido' => $pedido->id])}}"> Editar</a></td>
+                                <td><a href="{{ route('pedido.edit', ['pedido' => $pedido->id ]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
-                {{ $pedidos->appends($request)->links() }} <!-- botão de paginação, salvando as consultas do request em todas as páginas -->
                 
-                
+                {{ $pedidos->appends($request)->links() }}
+
                 <!--
                 <br>
                 {{ $pedidos->count() }} - Total de registros por página
                 <br>
-                {{ $pedidos->total() }} - Total de registros encontrados
+                {{ $pedidos->total() }} - Total de registros da consulta
                 <br>
                 {{ $pedidos->firstItem() }} - Número do primeiro registro da página
                 <br>
                 {{ $pedidos->lastItem() }} - Número do último registro da página
-                !-->
 
+                -->
+                <br>
                 Exibindo {{ $pedidos->count() }} pedidos de {{ $pedidos->total() }} (de {{ $pedidos->firstItem() }} a {{ $pedidos->lastItem() }})
             </div>
         </div>
+
     </div>
 
 @endsection
